0d162447d6fa63b0911d0bbf952080152411db9f
Alexey1254 <alxkill54@gmail.com>
2022-12-15 16:34:34 +0000

    pruebas

--- INFORMES/UT4/pgapp/src/index.php
+++ INFORMES/UT4/pgapp/src/index.php
@@ -6,10 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="data:,">
-    <title>Document</title>
+    <title>Travelroad</title>
 </head>
 <body>
-    
+    <h1>My travel Bucket list</h1>
+
+    <h2>Places I'd like to visit</h2>
+
+    <h2>Places I've Already been To</h2>
+
+
 </body>
 </html>
 <?php
@@ -17,7 +23,7 @@ $postgre = pg_connect("host=$host port=$port dbname=$dbname user=$user password=
 
 $result = pg_query($postgre, "SELECT * FROM places");
 
-echo "ppepee";
+var_dump($result);
 
 
 
