fb383c63f63d1f26a3fbffd47d762818a8ad93f6
Alexey1254 <alxkill54@gmail.com>
2022-12-15 17:16:54 +0000

    pruebas

--- INFORMES/UT4/pgapp/src/index.php
+++ INFORMES/UT4/pgapp/src/index.php
@@ -8,16 +8,6 @@
     <link rel="icon" href="data:,">
     <title>Travelroad</title>
 </head>
-<body>
-    <h1>My travel Bucket list</h1>
-
-    <h2>Places I'd like to visit</h2>
-
-    <h2>Places I've Already been To</h2>
-
-
-</body>
-</html>
 <?php
 $postgre = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
@@ -29,13 +19,31 @@ $toVisit = [];
 for($i = 0; $i < count($arrayResult); $i++) {
     if($arrayResult[$i]["visited"] === "t") {
         array_push($visited, $arrayResult[$i]);
+    } else {
+        array_push($toVisit, $arrayResult[$i]);
     }
 }
-
-var_dump($visited);
-
-//var_dump(pg_fetch_all($result));
-
-
-
 ?>
+<body>
+    <h1>My travel Bucket list</h1>
+
+    <h2>Places I'd like to visit</h2>
+    <ul>
+            <?php 
+            for ($i=0; $i < count($toVisit) ; $i++) { 
+                echo "<li>".$toVisit[$i]["name"]."</li>";
+            }
+            ?>
+        </ul>
+    <h2>Places I've Already been To</h2>
+    <ul>
+            <?php 
+            for ($i=0; $i < count($visited) ; $i++) { 
+                echo "<li>".$visited[$i]["name"]."</li>";
+            }
+            ?>
+        </ul>
+
+</body>
+</html>
+
